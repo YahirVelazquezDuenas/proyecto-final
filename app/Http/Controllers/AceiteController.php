069a47293b09e12f6690cf81337a89aaac7d1266
Yahir_Velazquez_Duenas <yahir.velazquez5481@alumnos.udg.mx>
2023-09-18 15:33:54 -0600

    Agregando Modelo, Controlador, Resource y Rama CRUD

---
+++ app/Http/Controllers/AceiteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aceite;
+use Illuminate\Http\Request;
+
+class AceiteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Aceite $aceite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Aceite $aceite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Aceite $aceite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Aceite $aceite)
+    {
+        //
+    }
+}
