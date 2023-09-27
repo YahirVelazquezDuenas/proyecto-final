06b715a749aa9db65e4ea3d51d97466a9e0044f8
experthor <brandon.rito5477@alumnos.udg.mx>
2023-09-27 15:31:55 -0600

    confirmando

--- resources/views/compras/indexCompras.blade.php
+++ resources/views/compras/indexCompras.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Principal de compra</title>
+    <title>Principal de compras</title>
 </head>
 <body>
     <h1>Encabezados de compras</h1>
