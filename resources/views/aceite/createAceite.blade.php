25127bde815fb1da0d3b8aab80470ffef9de1ace
Yahir_Velazquez_Duenas <yahir.velazquez5481@alumnos.udg.mx>
2023-09-28 16:04:37 -0600

    pruebas de algo para ver el push

--- resources/views/aceite/createAceite.blade.php
+++ resources/views/aceite/createAceite.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear Aceite</title>
+    <title>Crear Aceites</title>
 </head>
 <body>
     <h1>Registro de aceites</h1>
