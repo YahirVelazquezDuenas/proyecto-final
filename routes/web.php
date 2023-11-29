787c2a35c08e06942f89a586bc873c515889b5ed
sami-sopas <ramirezcuenca@outlook.es>
2023-11-29 10:21:08 -0600

    ClienteController now working

--- routes/web.php
+++ routes/web.php
@@ -30,28 +30,41 @@ Route::get('/signup', [UserController::class, 'showSignupForm'])->name('signup.f
 Route::post('/signup', [UserController::class, 'registerUser'])->name('registro.usuario');
 
 Route::middleware(['auth'])->group(function() {
+
     //Rutas de aceites
     Route::resource('aceite', AceiteController::class);
     Route::get('aceite-descarga/{aceite}', [AceiteController::class, 'descargar'])->name('aceite.descarga');
     Route::delete('/aceite/{aceite}/eliminar-archivo', [AceiteController::class, 'destroyArchivo'])->name('aceite.eliminar-archivo');
+    //Route::get('aceite-descarga/{aceite}', [AceiteController::class, 'descargar'])->name('aceite.descarga');
+    // Route::get('/aceite/{id}/edit', 'AceiteController@edit')->name('editAceite');
+    // Route::put('/aceite/{id}', 'AceiteController@update')->name('updateAceite');
+    // Route::delete('/aceite/{id}', 'AceiteController@destroy')->name('destroyAceite');
+    // Route::delete('/aceite/{aceite}/eliminar-archivo', [AceiteController::class, 'destroyArchivo'])->name('aceite.eliminar-archivo');
+    //Route::post('/aceite', 'AceiteController@store')->name('createAceite');
+    //Route::get('/aceite/{id}', 'AceiteController@show')->name('showAceite');
 
+    //Rutas de clientes
     Route::resource('cliente', ClienteController::class);
-    Route::resource('compras', ComprasController::class);
-    Route::get('/cliente/{id}', 'ClienteController@show')->name('showCliente');
-    Route::get('/compras/{id}', 'ComprasController@show')->name('showCompras');
+    // Route::get('/cliente/{id}', 'ClienteController@show')->name('showCliente');
+    // Route::get('/compras/{id}', 'ComprasController@show')->name('showCompras');
+    // Route::post('/cliente', 'ClienteController@store')->name('createCliente');
+    // Route::get('/cliente/{id}/edit', 'ClienteController@edit')->name('editCliente');
+    // Route::put('/cliente/{id}', 'ClienteController@update')->name('updateCliente');
+    // Route::delete('/cliente/{id}', 'ClienteController@destroy')->name('destroyCliente');
 
-    Route::get('/cliente/{id}/edit', 'ClienteController@edit')->name('editCliente');
-    Route::put('/cliente/{id}', 'ClienteController@update')->name('updateCliente');
-    Route::delete('/cliente/{id}', 'ClienteController@destroy')->name('destroyCliente');
+    //Rutas de compras
+    Route::resource('compras', ComprasController::class);
+
     Route::get('/compras/{id}/edit', 'ComprasController@edit')->name('editCompras');
     Route::put('/compras/{id}', 'ComprasController@update')->name('updateCompras');
     Route::delete('/compras/{id}', 'ComprasController@destroy')->name('destroyCompras');
-    Route::post('/cliente', 'ClienteController@store')->name('createCliente');
+
     Route::post('/compras', 'ComprasController@store')->name('createCompras');    
 });
 //Route::get('/aceite/{id}', 'AceiteController@show')->name('showAceite');
 Route::post('/logout', [UserController::class, 'destroy'])->name('logout');
 Route::get('/landing',[WelcomeController::class, 'index'])->name('landing');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
